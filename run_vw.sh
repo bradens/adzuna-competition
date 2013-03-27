f8c648b03e3dc73872593995ca6099045369d040
Braden Simpson <braden@uvic.ca>
2013-03-27 13:31:00 -0700

    add some more stuff

--- run_vw.sh
+++ run_vw.sh
@@ -2,9 +2,9 @@
 
 # a script for running validation
 
-vw -d results/train.vw -c -k -f model --passes 20 --l1 0.0000001
+vw -d results/train.vw -c -k -f model --passes 20
 
-vw -t -d results/valid.vw -c -i model -p results/p.txt --l1 0.0000001
+vw -t -d results/valid.vw -c -i model -p results/p.txt
 
 python mae.py results/valid.vw results/p.txt
 
